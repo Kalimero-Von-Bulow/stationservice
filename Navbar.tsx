0a48ba91caf9221e240373e4e4f37359dafb7b8b
KaliMerO666 <kalimero@ik.me>
2026-02-26 15:33:30 +0100

    Add files via upload

--- Navbar.tsx
+++ Navbar.tsx
@@ -8,11 +8,12 @@ const navLinks: NavLink[] = [
   { label: "Works", href: "https://kalimero-works.vercel.app/", external: true },
   { label: "Rock, Ghost, Rock !", href: "https://rockghostsrock.digitalpress.blog/", external: true },
   { label: "PRNGRFX", href: "https://xxx19.digitalpress.blog/", external: true },
-  { label: "Censored Works", href: "https://kalimero666.gumroad.com/l/wqynnc", external: true },
-  { label: "PRNGRFX PACK #01", href: "https://kalimero666.gumroad.com/l/lkcdl", external: true },
+  { label: "Shop", href: "https://ko-fi.com/kalimero_666/shop", external: true },
+  { label: "Censored Works", href: "https://kalimero666.gumroad.com/l/lkcdl", external: true },
+  { label: "PRNGRFX #01", href: "https://kalimero666.gumroad.com/l/wqynnc", external: true },
   { label: "Art Majeur", href: "https://www.artmajeur.com/olivier-kalimero-quemere", external: true },
   { label: "Goodies", href: "https://kalimero.myspreadshop.fr/", external: true },
-  { label: "Shop", href: "https://ko-fi.com/kalimero_666/shop", external: true },
+  { label: "Order Your Portrait", href: "https://www.notion.so/Commande-ton-portrait-2efc6f12db2480b4b4bed88fcff659a7", external: true },
 ];
 
 const S = {
